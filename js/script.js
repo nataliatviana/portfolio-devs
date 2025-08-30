d97f672fb16522071325b4b494982c58dbed2a1e
Júlia Andrade <julia.andrade@maisunifacisa.com.br>
2025-08-30 18:26:52 -0300

    Adição javascript ao portifólio 2(Júlia) e novas estilizações

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+const formulario = document.getElementById('formulario');
 
+formulario.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const emailDestinatario = 'juliahenriquesdeandrade@gmail.com';
+    
+    const assuntoEmail  = `Portifólio`;
+    const mensagemEmail = `Olá, meu nome é ${nome}.\n${mensagem}`;
+    
+    const url = `mailto:${emailDestinatario}?subject=${encodeURIComponent(assuntoEmail)}&body=${encodeURIComponent(mensagemEmail)}` ;
+    window.open(url, '_blank');
+});
